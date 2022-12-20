9123830b7e33d1ef81023c5468d4f28a11b30711
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-12-20 13:37:02 +0300

    Update hocam cevap yollayabilirmisiniz .c

--- hocam cevap yollayabilirmisiniz .c
+++ hocam cevap yollayabilirmisiniz .c
@@ -1,1 +1,25 @@
-nkıyıkı
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+
+
+
+int main()
+{
+    typedef union
+    {
+        char name[20];
+        int flag;
+        float ondalik;
+    } data;
+
+    data veri;
+    strcpy(veri.name,"akif");
+    veri.flag = 1;
+    veri.ondalik = 1.5;
+    printf("%s",veri.name);
+    printf("%d",veri.flag);
+    printf("%f",veri.ondalik);
+    return 0;
+}
