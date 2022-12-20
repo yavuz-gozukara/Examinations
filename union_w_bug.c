2f2ff9088a8097e75f341fce2e0cbff3e31cb3ee
Adem Buran <116068005+AdBuran@users.noreply.github.com>
2022-12-20 13:38:04 +0300

    ademunionbug

--- union_w_bug.c
+++ union_w_bug.c
@@ -1,20 +1,19 @@
-// There is a bug 
-
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
-
-int main()
-{
-    typedef union
+typedef union
     {
         char name[10];
         int flag;
         float ondalik;
     } data;
+int main()
+{
+    
     
     data veri;
-    veri.name = "Akif";
+    
+    strcpy(veri.name, "Akif");
     printf("Name = %s\n", veri.name);
     veri.flag = 1;
     printf("flag = %d\n", veri.flag);
