95a9b63c843555bddbfdc05193677163025a7b03
yavuz selim gozukara <78623551+yavuz-gozukara@users.noreply.github.com>
2022-12-20 13:43:48 +0300

    Update hocam cevap yollayabilirmisiniz .c

--- hocam cevap yollayabilirmisiniz .c
+++ hocam cevap yollayabilirmisiniz .c
@@ -1,25 +1,23 @@
 #include "stdio.h"
+#include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
-
-
-
 
 int main()
 {
     typedef union
     {
-        char name[20];
+        char name[10];
         int flag;
         float ondalik;
     } data;
-
+    
     data veri;
-    strcpy(veri.name,"akif");
+	strcpy(veri.name,"yavuz");
+    printf("Name = %s\n", veri.name);
     veri.flag = 1;
+    printf("flag = %d\n", veri.flag);
     veri.ondalik = 1.5;
-    printf("%s",veri.name);
-    printf("%d",veri.flag);
-    printf("%f",veri.ondalik);
+    printf("f = %f\n", veri.ondalik);
     return 0;
 }
